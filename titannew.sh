ac21dee14950f508b9bf61f1984e92ba37500e68
Mr Dikci <97982648+Dikci@users.noreply.github.com>
2025-02-09 21:05:24 +0300

    Update titannew.sh

--- titannew.sh
+++ titannew.sh
@@ -6,8 +6,6 @@ sudo ufw allow 48710
 docker stop titan-edge-container
 docker rm titan-edge-container
 
-#!/bin/bash
-
 # Pull the Titan Docker image
 docker pull nezha123/titan-edge
 
@@ -19,3 +17,13 @@ cd ~/.titanedge
 
 # Run the Titan Docker container with a specific name
 docker run --network=host -d -v ~/.titanedge:/root/.titanedge --name titan-edge-container nezha123/titan-edge
+
+# Prompt for Titan identity code
+echo -e "Enter your Titan identity code: "
+read identity_code  # Read input from the user
+
+# Bind the identity code to the device
+docker run --rm -it -v ~/.titanedge:/root/.titanedge nezha123/titan-edge bind --hash="$identity_code" https://api-test1.container1.titannet.io/api/v2/device/binding
+
+# Success message
+echo "Titan identity successfully bound!"
