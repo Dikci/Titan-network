8379ad48b8c29738dd124e23a103c37fddc824e2
Mr Dikci <97982648+Dikci@users.noreply.github.com>
2025-02-09 20:14:39 +0300

    Create titannew.sh

---
+++ titannew.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+sudo ufw allow 1234
+sudo ufw allow 55702
+sudo ufw allow 48710
+
+#!/bin/bash
+
+# Pull the Titan Docker image
+docker pull nezha123/titan-edge
+
+# Create the Titan directory
+mkdir -p ~/.titanedge
+
+# Change to that directory
+cd ~/.titanedge
+
+# Run the Titan Docker container with a specific name
+docker run --network=host -d -v ~/.titanedge:/root/.titanedge --name titan-edge-container nezha123/titan-edge
